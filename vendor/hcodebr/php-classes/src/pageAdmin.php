4d062025643f6b96cabe78b1337c16939bddca28
David <dmsm2000@hotmail.com>
2020-10-19 18:47:05 +0100

    Page Admin

---
+++ vendor/hcodebr/php-classes/src/pageAdmin.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Hcode;
+
+class PageAdmin extends Page {
+
+    public function __contruct($opts = array(), $tpl_dir = "/Projeto/views/admin/"){
+        parent::__contruct($opts, $tpl_dir);
+    }
+
+}
+
+
+?>

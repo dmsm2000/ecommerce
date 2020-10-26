594a0091524b1e90ec4c998245448425534320cb
David <dmsm2000@hotmail.com>
2020-10-26 18:28:39 +0000

    products

--- vendor/hcodebr/php-classes/src/Model.php
+++ vendor/hcodebr/php-classes/src/Model.php
@@ -4,32 +4,31 @@ namespace Hcode;
 
 class Model {
 
-    private $values = array();
+    private $values = [];
 
-    public function __call($name, $args) {
-        $method = substr($name, 0, 3);
+	public function __call($name, $args) {
+		$method = substr($name, 0, 3);
         $fieldName = substr($name, 3, strlen($name));
+        
+		switch ($method) {
+			case "get":
+				return (isset($this->values[$fieldName])) ? $this->values[$fieldName] : 0;
+			break;
+			case "set":
+				$this->values[$fieldName] = $args[0];
+			break;
+		}
+	}
 
-        switch($method) {
-            case "get":
-                return (isset($this->values[$fieldName])) ? $this->values[$fieldName] : NULL;
-                break;
-            case "set":
-                $this->values[$fieldName] = $args[0];
-                break;
-        }
-    }
+	public function setData($data = array()) {
+		foreach ($data as $key => $value) {
+			$this->{"set".$key}($value);
+		}
+	}
 
-    public function setData($data = array()) {
-        foreach($data as $key=>$value) {
-            $this->{"set".$key}($value);
-        }
-    }
-    
-
-    public function getValues() {
-        return $this->values;
-    }
+	public function getValues() {
+		return $this->values;
+	}
 
 }
 
